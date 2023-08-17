1245f43ffa2adcb83c0b4117aec643888fadbf2f
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-08-17 14:15:01 +0300

    added some styles by MUI

--- src/app/AppWithRedux.tsx
+++ src/app/AppWithRedux.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.css';
 import {  TaskType } from '../api/todoListAPI';
 import { TodoListsList } from '../features/TodoListsList/TodoListsList';
-
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export type TasksStateType = {
   [key: string]: Array<TaskType>
@@ -11,6 +16,23 @@ export type TasksStateType = {
 function AppWithRedux() {
   return (
     <div className="App">
+       <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <TodoListsList/>
     </div>
   );
--- src/features/TodoListsList/TodoListsList.tsx
+++ src/features/TodoListsList/TodoListsList.tsx
@@ -7,71 +7,85 @@ import { TodoList } from './TodoList/Todolist';
 import { TaskStatuses } from '../../api/todoListAPI';
 import { addTaskTC, removeTaskTC, updateTaskTC } from './tasksReducer';
 import { FilterValuesType, TodoListDomainType, addTodoListTC, changeTodoListFilterAC, changeTodoListTitleTC, getTodosTC, removeTodoListTC } from './todoListsReducer';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 
 
 
 export const TodoListsList: React.FC = () => {
-    // useAppSelector - is a custom hook based on built-in useSelector
-    // with useAppSelector we reduced our types, moving AppRootStateType to store
-  
-    const todoLists = useAppSelector<Array<TodoListDomainType>>(state => state.todoLists)
-    const tasks = useAppSelector<TasksStateType>(state => state.tasks)
-    const dispatch = useAppDispatch()
-  
-    const addTodoList = useCallback((newTodoListTitle: string) => {
-      dispatch(addTodoListTC(newTodoListTitle))
-    }, [])
-    const removeTodoList = useCallback((todoListId: string) => {
-      dispatch(removeTodoListTC(todoListId))
-    }, [])
-    const removeTask = useCallback((todoListId: string, taskId: string) => {
-      dispatch(removeTaskTC(todoListId, taskId))
-    }, [])
-    const changeFilterValue = useCallback((todoListId: string, value: FilterValuesType) => {
-      const action = changeTodoListFilterAC(todoListId, value)
-      dispatch(action)
-    }, [])
-    const addTask = useCallback((todoListId: string, newTaskTitle: string) => {
-      dispatch(addTaskTC(todoListId, newTaskTitle))
-    }, [])
-    const changeTaskStatus = useCallback((todoListId: string, taskId: string, status: TaskStatuses) => {
-      dispatch(updateTaskTC(todoListId, taskId, { status }))
-  
-    }, [])
-    const changeTaskTitle = useCallback((todoListId: string, taskId: string, value: string) => {
-      dispatch(updateTaskTC(todoListId, taskId, { title: value }))
-    }, [])
-    const changeTodoListTitle = useCallback((todoListId: string, newTitle: string) => {
-      dispatch(changeTodoListTitleTC(todoListId, newTitle))
-    }, [])
-  
-    useEffect(() => {
-      dispatch(getTodosTC())
-    }, [])
-  
-    return (
-      <>
-        <AddItemForm addItem={addTodoList} />
-        {
-          todoLists.map(tl => {
-            return (
-              <TodoList
-                key={tl.id}
-                id={tl.id}
-                title={tl.title}
-                filter={tl.filter}
-                tasks={tasks[tl.id]}
-                addTask={addTask}
-                removeTask={removeTask}
-                removeTodoList={removeTodoList}
-                changeFilterValue={changeFilterValue}
-                changeTaskStatus={changeTaskStatus}
-                changeTaskTitle={changeTaskTitle}
-                changeTodoListTitle={changeTodoListTitle}
-              />
-            )
-          })
-        }</>
-    )
-  }
+  // useAppSelector - is a custom hook based on built-in useSelector
+  // with useAppSelector we reduced our types, moving AppRootStateType to store
+
+  const todoLists = useAppSelector<Array<TodoListDomainType>>(state => state.todoLists)
+  const tasks = useAppSelector<TasksStateType>(state => state.tasks)
+  const dispatch = useAppDispatch()
+
+  const addTodoList = useCallback((newTodoListTitle: string) => {
+    dispatch(addTodoListTC(newTodoListTitle))
+  }, [])
+  const removeTodoList = useCallback((todoListId: string) => {
+    dispatch(removeTodoListTC(todoListId))
+  }, [])
+  const removeTask = useCallback((todoListId: string, taskId: string) => {
+    dispatch(removeTaskTC(todoListId, taskId))
+  }, [])
+  const changeFilterValue = useCallback((todoListId: string, value: FilterValuesType) => {
+    const action = changeTodoListFilterAC(todoListId, value)
+    dispatch(action)
+  }, [])
+  const addTask = useCallback((todoListId: string, newTaskTitle: string) => {
+    dispatch(addTaskTC(todoListId, newTaskTitle))
+  }, [])
+  const changeTaskStatus = useCallback((todoListId: string, taskId: string, status: TaskStatuses) => {
+    dispatch(updateTaskTC(todoListId, taskId, { status }))
+
+  }, [])
+  const changeTaskTitle = useCallback((todoListId: string, taskId: string, value: string) => {
+    dispatch(updateTaskTC(todoListId, taskId, { title: value }))
+  }, [])
+  const changeTodoListTitle = useCallback((todoListId: string, newTitle: string) => {
+    dispatch(changeTodoListTitleTC(todoListId, newTitle))
+  }, [])
+
+  useEffect(() => {
+    dispatch(getTodosTC())
+  }, [])
+
+  return (
+    <>
+      <Container fixed>
+        <Grid container style={{padding: '20px'}}>
+          <AddItemForm addItem={addTodoList} />
+        </Grid>
+        <Grid container spacing={3}>
+          {
+            todoLists.map(tl => {
+              return (
+                <Grid item>
+                  <Paper style={{padding: '10px'}}>
+                  <TodoList
+                    key={tl.id}
+                    id={tl.id}
+                    title={tl.title}
+                    filter={tl.filter}
+                    tasks={tasks[tl.id]}
+                    addTask={addTask}
+                    removeTask={removeTask}
+                    removeTodoList={removeTodoList}
+                    changeFilterValue={changeFilterValue}
+                    changeTaskStatus={changeTaskStatus}
+                    changeTaskTitle={changeTaskTitle}
+                    changeTodoListTitle={changeTodoListTitle}
+                  />
+                  </Paper>
+                </Grid>
+              )
+            })
+          }
+        </Grid>
+      </Container>
+    </>
+  )
+}
